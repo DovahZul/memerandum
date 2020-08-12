622487cb4c661352e07c9df3502eb61c5793c726
dovahzul <tanayris278@gmail.com>
2020-08-12 13:22:50 +0300

    feat:\ added onclick

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import WeatherDisplay, { PLACES } from './Weather.js'
+//import WeatherDisplay, { PLACES } from './Weather.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <WeatherDisplay zip={"12345"} />
+      {PLACES.map((place, index) => (
+        <button
+        key={index}
+        onClick={() => {
+          console.log('Clicked index '+index);
+        }}
+        >
+        {place.name}
+        </button>
+        ))}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        );
+    }
   }
-}
 
-  export default App;
+  class WeatherDisplay extends Component {
+    render() {
+      return (
+        <h1>Displaying weather for city {this.props.zip}</h1>
+        );
+      }
+    }
+
+    export const PLACES = [
+    { name: "Palo Alto", zip: "94303" },
+    { name: "San Jose", zip: "94088" },
+    { name: "Santa Cruz", zip: "95062" },
+    { name: "Honolulu", zip: "96803" }
+    ];
+
+    export default App;
